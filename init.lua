d4fcdc4967bb36b35bc2e7b6d901c2c1d61884a8
Unknown <pumpkin650@gmail.com>
2017-02-12 13:28:24 +0100

    new shield and boat texture

--- init.lua
+++ init.lua
@@ -1436,7 +1436,7 @@ mobs:register_egg("amc:95witch", "Witch", "witch_inv.png", 0)
 --################### BOAT
 --###################
 
-mobs:register_mob("amc:99boat", {
+mobs:register_mob("amc:97boat", {
 	type = "animal",
 	passive = true,
     runaway = true,
@@ -1461,4 +1461,35 @@ mobs:register_mob("amc:99boat", {
 	},
 })
 
-mobs:register_egg("amc:99boat", "Boat", "boat_inv.png", 0)
+mobs:register_egg("amc:97boat", "Boat", "boat_inv.png", 0)
+
+--###################
+--################### SHIELD
+--###################
+
+mobs:register_mob("amc:98shield", {
+	type = "animal",
+	passive = true,
+    runaway = true,
+    stepheight = 1.2,
+	hp_min = 30,
+	hp_max = 60,
+	armor = 150,
+    collisionbox = {-0.35, -0.01, -0.35, 0.35, 2, 0.35},
+    rotate = -180,
+	visual = "mesh",
+	mesh = "shield.b3d",
+    textures = {{"shield.png"}},
+	visual_size = {x=3, y=3},
+	walk_velocity = 0.6,
+	run_velocity = 2,
+	jump = true,
+	animation = {
+		speed_normal = 25,		speed_run = 50,
+		stand_start = 40,		stand_end = 80,
+		walk_start = 0,		walk_end = 40,
+		run_start = 0,		run_end = 40,
+	},
+})
+
+mobs:register_egg("amc:98shield", "Shield", "shield_inv.png", 0)
