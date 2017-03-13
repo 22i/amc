fa18ff57227e8cc9c9fa7d5b22f18a8bfe43fc71
Unknown <pumpkin650@gmail.com>
2017-03-13 08:45:40 +0100

    guardian, horse new minecart
    guardian eye position and animation
    skeleton horse tail fixed

--- init.lua
+++ init.lua
@@ -491,9 +491,9 @@ mobs:register_mob("amc:16guardian", {
 	jump = true,
 	animation = {
 		speed_normal = 25,		speed_run = 50,
-		stand_start = 0,		stand_end = 40,
-		walk_start = 0,		walk_end = 40,
-		run_start = 0,		run_end = 40,
+		stand_start = 0,		stand_end = 20,
+		walk_start = 0,		walk_end = 20,
+		run_start = 0,		run_end = 20,
 	},
 })
 
@@ -524,9 +524,9 @@ mobs:register_mob("amc:17guardian_elder", {
 	jump = true,
 	animation = {
 		speed_normal = 25,		speed_run = 50,
-		stand_start = 0,		stand_end = 40,
-		walk_start = 0,		walk_end = 40,
-		run_start = 0,		run_end = 40,
+		stand_start = 0,		stand_end = 20,
+		walk_start = 0,		walk_end = 20,
+		run_start = 0,		run_end = 20,
 	},
 })
 
@@ -1555,3 +1555,34 @@ mobs:register_mob("amc:84chest", {
 })
 
 mobs:register_egg("amc:84chest", "Chest", "normal_inv.png", 0)
+
+--###################
+--################### MINECART
+--###################
+
+mobs:register_mob("amc:85minecart", {
+	type = "animal",
+	passive = true,
+    runaway = true,
+    stepheight = 1.2,
+	hp_min = 30,
+	hp_max = 60,
+	armor = 150,
+    collisionbox = {-0.35, -0.01, -0.35, 0.35, 2, 0.35},
+    rotate = -180,
+	visual = "mesh",
+	mesh = "minecart.b3d",
+    textures = {{"minecart.png"}},
+	visual_size = {x=12, y=12},
+	walk_velocity = 0.6,
+	run_velocity = 2,
+	jump = true,
+	animation = {
+		speed_normal = 25,		speed_run = 50,
+		stand_start = 20,		stand_end = 25,
+		walk_start = 0,		walk_end = 20,
+		run_start = 0,		run_end = 20,
+	},
+})
+
+mobs:register_egg("amc:85minecart", "Minecart", "minecart_normal.png", 0)
