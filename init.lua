0692afd05983edef49747bc3140e9955c2ca688d
Unknown <pumpkin650@gmail.com>
2017-04-28 12:56:33 +0200

    new bed model

--- init.lua
+++ init.lua
@@ -711,15 +711,15 @@ mobs:register_mob("amc:23rabbit", {
 	visual = "mesh",
 	mesh = "rabbit.b3d",
     textures = {{"rabbit.png"},{"rabbit1.png"},{"rabbit2.png"},{"rabbit3.png"},{"rabbit4.png"},{"rabbit5.png"},{"rabbit6.png"},{"rabbit7.png"}},
-	visual_size = {x=3, y=3},
+	visual_size = {x=1.5, y=1.5},
 	walk_velocity = 0.6,
 	run_velocity = 2,
 	jump = true,
 	animation = {
 		speed_normal = 25,		speed_run = 50,
-		stand_start = 40,		stand_end = 80,
-		walk_start = 0,		walk_end = 40,
-		run_start = 0,		run_end = 40,
+		stand_start = 0,		stand_end = 0,
+		walk_start = 0,		walk_end = 20,
+		run_start = 0,		run_end = 20,
 	},
 })
 
@@ -1211,6 +1211,7 @@ mobs:register_mob("amc:51pigman", {
 		stand_start = 40,		stand_end = 80,
 		walk_start = 0,		walk_end = 40,
 		run_start = 0,		run_end = 40,
+        punch_start = 90,		punch_end = 130,
 	},
 })
 
@@ -1651,3 +1652,34 @@ mobs:register_mob("amc:85minecart", {
 })
 
 mobs:register_egg("amc:85minecart", "Minecart", "minecart_normal.png", 0)
+
+--###################
+--################### BED
+--###################
+
+mobs:register_mob("amc:86bed", {
+	type = "animal",
+	passive = true,
+    runaway = true,
+    stepheight = 1.2,
+	hp_min = 30,
+	hp_max = 60,
+	armor = 150,
+    collisionbox = {-0.35, -0.01, -0.35, 0.35, 2, 0.35},
+    rotate = -180,
+	visual = "mesh",
+	mesh = "bed.b3d",
+    textures = {{"light_blue.png"},{"green.png"},{"gray.png"},{"cyan.png"},{"brown.png"},{"blue.png"},{"black.png"},{"yellow.png"},{"lime.png"},{"magenta.png"},{"orange.png"},{"pink.png"},{"purple.png"},{"red.png"},{"silver.png"},{"white.png"}},
+	visual_size = {x=3, y=3},
+	walk_velocity = 0.6,
+	run_velocity = 2,
+	jump = true,
+	animation = {
+		speed_normal = 25,		speed_run = 50,
+		stand_start = 20,		stand_end = 25,
+		walk_start = 0,		walk_end = 20,
+		run_start = 0,		run_end = 20,
+	},
+})
+
+mobs:register_egg("amc:86bed", "Bed", "bed.png", 0)
