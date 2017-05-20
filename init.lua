01dbd08541d9c58ffd875610dfb17c33041d8371
Unknown <pumpkin650@gmail.com>
2017-05-20 15:52:10 +0200

    changed spider, enderdragon flies and new agent added

--- init.lua
+++ init.lua
@@ -398,9 +398,9 @@ mobs:register_mob("amc:12enderdragon", {
 	jump = true,
 	animation = {
 		speed_normal = 25,		speed_run = 50,
-		stand_start = 40,		stand_end = 80,
-		walk_start = 0,		walk_end = 40,
-		run_start = 0,		run_end = 40,
+		stand_start = 0,		stand_end = 20,
+		walk_start = 0,		walk_end = 20,
+		run_start = 0,		run_end = 20,
 	},
 })
 
@@ -1505,7 +1505,38 @@ mobs:register_mob("amc:62witch", {
 	},
 })
 
-mobs:register_egg("amc:62witch", "Witch", "witch_inv.png", 0)
+--###################
+--################### AGENT
+--###################
+
+mobs:register_mob("amc:63agent", {
+	type = "animal",
+	passive = true,
+    runaway = true,
+    stepheight = 1.2,
+	hp_min = 30,
+	hp_max = 60,
+	armor = 150,
+    collisionbox = {-0.35, -0.01, -0.35, 0.35, 2, 0.35},
+    rotate = -180,
+	visual = "mesh",
+	mesh = "agent.b3d",
+	textures = {
+		{"agent.png"},
+	},
+	visual_size = {x=3, y=3},
+	walk_velocity = 0.6,
+	run_velocity = 2,
+	jump = true,
+	animation = {
+		speed_normal = 25,		speed_run = 50,
+		stand_start = 20,		stand_end = 60,
+		walk_start = 0,		walk_end = 20,
+		run_start = 0,		run_end = 20,
+	},
+})
+
+mobs:register_egg("amc:63agent", "Agent", "agent_inv.png", 0)
 
 --###################
 --################### BOAT
