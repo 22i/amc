616bcae69141bb84e579d460e77e46a8d9f803c2
Unknown <pumpkin650@gmail.com>
2017-08-21 22:38:00 +0200

    mooshrom uses 2 textures, zombie and zvillager texture fix
    -zombie villager right and left arm texture mapping fix
    
    -zombie right and left arm texture mapping fix left backleg
    
    -removed 2textures test and slime test

--- init.lua
+++ init.lua
@@ -85,9 +85,7 @@ mobs:register_mob("amc:0acow", {
     collisionbox = {-0.35, -0.01, -0.35, 0.35, 1.4, 0.35},
 	visual = "mesh",
 	mesh = "mobs_mc_cow.b3d",
-	textures = {
-		{"mobs_mc_cow.png"},
-	},
+	textures = {"mobs_mc_cow.png", "amc_mushroom_red1.png"}, -- amc_mushroom_red1 is empty and transparent 16x16 png it removes the white problem
     sounds = {
 		death = "amc_belieber"
 	},
@@ -123,10 +121,7 @@ mobs:register_mob("amc:0babycow", {
     collisionbox = {-0.35, -0.01, -0.35, 0.35, 1, 0.35},
 	visual = "mesh",
 	mesh = "mobs_mc_cow_baby.b3d",
-	textures = {
-		{"mobs_mc_cow.png"},
-        {"mobs_mc_mooshroom.png^mobs_mc_mooshroom_mooshroom.png"},
-	},
+	textures = {"mobs_mc_cow.png", "amc_mushroom_red1.png"},
 	visual_size = {x=1.5, y=1.5},
 	walk_velocity = 0.6,
 	run_velocity = 2,
@@ -159,9 +154,7 @@ mobs:register_mob("amc:0amooshroom", {
     collisionbox = {-0.35, -0.01, -0.35, 0.35, 1.4, 0.35},
 	visual = "mesh",
 	mesh = "mobs_mc_cow.b3d",
-	textures = {
-		{"mobs_mc_mooshroom.png^mobs_mc_mooshroom_mooshroom.png"},
-	},
+	textures = {"mobs_mc_mooshroom.png", "amc_mushroom_red.png"},
 	visual_size = {x=3, y=3},
 	walk_velocity = 0.6,
 	run_velocity = 2,
@@ -179,6 +172,39 @@ mobs:register_mob("amc:0amooshroom", {
 })
 
 mobs:register_egg("amc:0amooshroom", "Mooshroom", "mobs_mc_spawn_icon_mooshroom.png", 0)
+
+--###################
+--################### BABY COW MOOSHROOM
+--###################
+
+mobs:register_mob("amc:0babymooshroom", {
+	type = "animal",
+	passive = true,
+    runaway = true,
+    stepheight = 1.2,
+	hp_min = 12,
+	hp_max = 12,
+    collisionbox = {-0.35, -0.01, -0.35, 0.35, 1, 0.35},
+	visual = "mesh",
+	mesh = "mobs_mc_cow_baby.b3d",
+	textures = {"mobs_mc_mooshroom.png", "amc_mushroom_red.png"},
+	visual_size = {x=1.5, y=1.5},
+	walk_velocity = 0.6,
+	run_velocity = 2,
+	jump = true,
+	animation = {
+        walk_speed = 25,
+        run_speed = 40,
+		stand_start = 0,
+        stand_end = 0,
+		walk_start = 0,
+        walk_end = 40,
+		run_start = 0,
+        run_end = 40,
+	},
+})
+
+mobs:register_egg("amc:0babymooshroom", "Baby Mooshroom", "mobs_mc_spawn_icon_mooshroom_baby.png", 0)
 
 --###################
 --################### CREEPER
@@ -2926,60 +2952,6 @@ mobs:register_mob("amc:63agent", {
 })
 
 mobs:register_egg("amc:63agent", "Agent 007", "mobs_mc_spawn_icon_agent.png", 0)
-
---###################
---################### 2 TEXTURES TEST
---###################
-
---textures = { --combine
---{"amc_1.png" and "amc_2.png"},
---{"amc_1.png", "amc_2.png"},
---{"amc_1.png"}, {"amc_2.png"},
---{"amc_1.png^amc_2.png"},
---{"amc_2.png^amc_1.png"},
---},
-
-mobs:register_mob("amc:z2texturetest", {
-	type = "animal",
-	passive = true,
-    runaway = true,
-    stepheight = 1.2,
-	hp_min = 12,
-	hp_max = 12,
-    collisionbox = {-0.35, -0.01, -0.35, 0.35, 1, 0.35},
-	visual = "mesh",
-	mesh = "amc_2texturetest.b3d",
-    textures = {"amc_2.png", "amc_1.png"},
-	visual_size = {x=3, y=3},
-	walk_velocity = 0.6,
-	run_velocity = 2,
-	jump = true,
-})
-
-mobs:register_egg("amc:z2texturetest", "2 Textures test", "amc_1.png", 0)
-
---###################
---################### 22 slime
---###################
-
-mobs:register_mob("amc:z2texslime", {
-	type = "animal",
-	passive = true,
-    runaway = true,
-    stepheight = 1.2,
-	hp_min = 12,
-	hp_max = 12,
-    collisionbox = {-0.35, -0.01, -0.35, 0.35, 1, 0.35},
-	visual = "mesh",
-	mesh = "slime_big.x",
-    textures = {"slime_outside.png","slime_inside.png","eye_right.png","eye_left.png","mouth.png"},
-	visual_size = {x=0.5, y=0.5},
-	walk_velocity = 0.6,
-	run_velocity = 2,
-	jump = true,
-})
-
-mobs:register_egg("amc:z2texslime", "2 Tex slime", "amc_1.png", 0)
 
 --###################
 --################### BOAT
